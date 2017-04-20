82209013e41495a6ba2854e3f098c5ea26ff94e4
Bhavan Patel <bhavan7@yahoo.com>
2017-04-20 09:02:07 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,6 @@ import { StyleSheet, Image, View, ScrollView, Text, TouchableHighlight } from 'r
 import PixAccordion from 'react-native-pixfactory-accordion';
 import { Actions, Router } from 'react-native-router-flux';
 import Data from './Data';
-const DATA = [
-  { id: 0, easing: 'easeOutSin', collapsed: true, text: 'Demo1' },
-  { id: 1, easing: 'easeOutSin', collapsed: true, text: 'Demo2' },
-  { id: 2, easing: 'easeOutSin', collapsed: true, text: 'Demo3' },
-  { id: 3, easing: 'easeOutSin', collapsed: true, text: 'Demo4' },
-  { id: 4, easing: 'easeOutSin', collapsed: true, text: 'Demo5' },
-  { id: 5, easing: 'easeOutSin', collapsed: true, text: 'Demo6' },
-  { id: 6, easing: 'easeOutSin', collapsed: true, text: 'Demo7' },
-  { id: 7, easing: 'easeOutSin', collapsed: true, text: 'Demo8' },
-  { id: 8, easing: 'easeOutSin', collapsed: true, text: 'Demo9' },
-  { id: 9, easing: 'easeOutSin', collapsed: true, text: 'Demo10' },
-  { id: 10, easing: 'easeOutSin', collapsed: true, text: 'Demo11' },
-  { id: 11, easing: 'easeOutSin', collapsed: true, text: 'Demo12' },
-];
 
 class Home extends Component {
 
